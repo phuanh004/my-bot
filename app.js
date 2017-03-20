8b220e8f2bfb2850e1a2126aaf58c0b768f273a9
Anh Pham <phuanh004@gmail.com>
2017-03-20 10:02:31 +0700

    Update server

--- app.js
+++ app.js
@@ -1,8 +1,5 @@
-const http = require('http');
-
-const express = require('express');
-
-const server = http.createServer(express);
+const app = require('express');
+const morgan  = require('morgan');
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
 var ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
@@ -10,3 +7,6 @@ var ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 server.listen(port, ip, function () {
     console.log( "Listening on " + server_ip_address + ", port " + server_port )
 });
+
+app.listen(port, ip);
+console.log('Server running on http://%s:%s', ip, port);
