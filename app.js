857ca5519ee7d502f3526310f3016a3b1b60d334
Anh Pham <phuanh004@gmail.com>
2017-03-20 11:18:57 +0700

    Update server

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ const morgan  = require('morgan');
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
 var ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
-app.use(function(err, req, res, next){
-    console.error(err.stack);
-    res.status(500).send('Something bad happened!');
-});
+// app.use(function(err, req, res, next){
+//     console.error(err.stack);
+//     res.status(500).send('Something bad happened!');
+// });
 
 app.listen(port, ip);
 console.log('Server running on http://%s:%s', ip, port);
