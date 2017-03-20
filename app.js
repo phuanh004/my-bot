0130d904fb589e00876f255775760c3525884025
Anh Pham <phuanh004@gmail.com>
2017-03-20 10:06:36 +0700

    Update server

--- app.js
+++ app.js
@@ -8,5 +8,10 @@ server.listen(port, ip, function () {
     console.log( "Listening on " + server_ip_address + ", port " + server_port )
 });
 
+app.use(function(err, req, res, next){
+    console.error(err.stack);
+    res.status(500).send('Something bad happened!');
+});
+
 app.listen(port, ip);
 console.log('Server running on http://%s:%s', ip, port);
