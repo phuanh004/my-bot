866d85a3050e0d9eb74448ddc8162d66160994a2
Anh Pham <phuanh004@gmail.com>
2017-03-20 09:16:02 +0700

    Update server

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const express = require('express');
 
 const server = http.createServer(express);
 
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
-server.listen(server_port, server_ip_address, function () {
+server.listen(port, ip, function () {
     console.log( "Listening on " + server_ip_address + ", port " + server_port )
 });
